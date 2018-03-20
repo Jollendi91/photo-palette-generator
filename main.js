33020708f54dacc59804becbdc9183acd2f83e2a
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-03-20 11:52:16 -0500

    add function stubs

---
+++ main.js
@@ -0,0 +1,15 @@
+function listenForPhotoSearchClick() {
+    //this listens for a click on the photo search form
+}
+
+function renderSearchResults() {
+    //this renders the search results on the page
+}
+
+function generateColorPalette() {
+    //this makes a call to the color extractor api and returns a set of colors in the photo
+}
+
+function listenForPhotoSelect() {
+    //this listens for a click on a search result photo
+}
