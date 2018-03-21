9588225ad37d641302f5927f293f2280ad27bd02
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-03-21 18:21:09 -0500

    add search listener and Unsplash API call

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+const UNSPLASH_URL = "https://api.unsplash.com/search/photos";
+
+function getSearchResults(searchText, callback) {
+    const settings = {
+        data: {
+           client_id: '8f38787e053192449fa982baa9609a354ef6e12dafc55c7d1166c486d77017a3', 
+           query: searchText,
+           per_page: 15
+        },
+        dataType: 'json',
+        success: callback,
+    }
+
+    $.ajax(UNSPLASH_URL, settings);
+
+}
+
+function getPaginationButtons(response, status, jqXHR) {
+    console.log(jqXHR.getResponseHeader('Link'));
+    console.log(response);
+}
+
 function listenForPhotoSearchClick() {
     //this listens for a click on the photo search form
+    $('#photo-search-form').submit(event => {
+    event.preventDefault();
+    console.log("Search form was submitted");
+    let searchTerm = $('#search-input').val();
+    $('#search-input').val('');
+    console.log(searchTerm);
+    getSearchResults(searchTerm, getPaginationButtons);
+    });
 }
 
 function renderSearchResults() {
@@ -13,3 +43,9 @@ function generateColorPalette() {
 function listenForPhotoSelect() {
     //this listens for a click on a search result photo
 }
+
+function callListeners() {
+    listenForPhotoSearchClick();
+}
+
+$(callListeners);
