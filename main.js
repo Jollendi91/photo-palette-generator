b711cf1c86109890bc136d1594656e4a173cea38
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-03-22 12:25:18 -0500

    get next page results

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 const UNSPLASH_SEARCH_URL = "https://api.unsplash.com/search/photos";
-const pageNumber = 1;
+let pageNumber = 1;
 
 function getSearchResults(searchText, callback) {
     const settings = {
         data: {
-           client_id: '8f38787e053192449fa982baa9609a354ef6e12dafc55c7d1166c486d77017a3', 
-           query: searchText,
-           per_page: 30,
-           page: 1
+            client_id: '8f38787e053192449fa982baa9609a354ef6e12dafc55c7d1166c486d77017a3',
+            query: searchText,
+            per_page: 30,
+            page: 1
         },
         dataType: 'json',
         success: callback,
     }
 
     $.ajax(UNSPLASH_SEARCH_URL, settings);
-
 }
 
 function renderPhoto(photo) {
@@ -28,22 +27,62 @@ function renderPhoto(photo) {
     `
 }
 
-function renderSearchResults(data) {
-    console.log(data);
-    const results= data.results.map((photo, index) => renderPhoto(photo));
+function parseLinkHeader(header) {
+    const parts = header.split(',');
+    const links = {};
+
+    parts.forEach(function (part) {
+        let section = part.split(';');
+        const url = section[0].replace(/<(.*)>/, '$1').trim();
+        const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+        links[name] = url;
+    })
+
+    return links;
+}
+
+function renderSearchResults(data, status, jqXHR) {
+    const pageLinks = jqXHR.getResponseHeader('Link');
+    const results = data.results.map((photo, index) => renderPhoto(photo));
     $('#search-results').html(results);
+
+    links = parseLinkHeader(pageLinks);
+
+    $('#page-buttons').html('');
+
+    if (links.prev) {
+        $('#page-buttons').append(`<button id="prev">Previous</button>`);
+    }
+
+    if (links.next) {
+        $('#page-buttons').append(`<button id="next">Next</button>`);
+    }
+}
+
+function listenForPaginationClick() {
+    const settings = {
+        success: renderSearchResults
+    }
+
+    $('#page-buttons').on('click', '#prev', event => {
+        $.ajax(links.prev, settings);
+    });
+
+    $('#page-buttons').on('click', '#next', event => {
+        $.ajax(links.next, settings);
+    });
 }
 
 
 function listenForPhotoSearchClick() {
     //this listens for a click on the photo search form
     $('#photo-search-form').submit(event => {
-    event.preventDefault();
-    console.log("Search form was submitted");
-    let searchTerm = $('#search-input').val();
-    $('#search-input').val('');
-    console.log(searchTerm);
-    getSearchResults(searchTerm, renderSearchResults);
+        event.preventDefault();
+        console.log("Search form was submitted");
+        let searchTerm = $('#search-input').val();
+        $('#search-input').val('');
+        console.log(searchTerm);
+        getSearchResults(searchTerm, renderSearchResults);
     });
 }
 
@@ -60,11 +99,11 @@ function getColorPalette(imageUrl) {
         "headers": {
             "Authorization": "Basic YWNjX2QxMmYyN2E4ZjU3MTFmNzoxZjdiYjViZWE5OGNkYjVkYWM0MGQyNGI0ODQ4OGIyYg==",
             "Cache-Control": "no-cache",
-          },
+        },
         success: generateColorPalette
     }
 
-   $.ajax(imageToBeExtracted);
+    $.ajax(imageToBeExtracted);
 }
 
 function generateColorPalette(er, data) {
@@ -74,7 +113,7 @@ function generateColorPalette(er, data) {
 
 function listenForPhotoSelect() {
     //this listens for a click on a search result photo
-    $('#search-results').on('click','img', event => {
+    $('#search-results').on('click', 'img', event => {
         const imageUrl = $('img').attr('src');
         getColorPalette(imageUrl);
     });
@@ -82,7 +121,8 @@ function listenForPhotoSelect() {
 
 function callListeners() {
     listenForPhotoSearchClick();
-  //  listenForPhotoSelect();
+    //  listenForPhotoSelect();
+    listenForPaginationClick();
 }
 
 $(callListeners);
