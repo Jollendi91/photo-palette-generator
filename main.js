4f392e700bbf8cc94ba1cf0032d992e0e700aaf3
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-03-27 20:06:57 -0500

    add particle background

--- main.js
+++ main.js
@@ -46,6 +46,12 @@ function parseLinkHeader(header) {
     return links;
 }
 
+function scrollToResults() {
+    $('html, body').animate({
+        scrollTop: ($('#search-results').offset().top)
+    },500);
+}
+
 function renderSearchResults(data, status, jqXHR) {
     const pageLinks = jqXHR.getResponseHeader('Link');
     if (data.results.length == 0) {
@@ -54,6 +60,7 @@ function renderSearchResults(data, status, jqXHR) {
         const results = data.results.map((photo, index) => renderPhoto(photo));
 
         $('#search-results').html(results);
+        scrollToResults();
 
         links = parseLinkHeader(pageLinks);
         $('.page-buttons').show();
@@ -93,8 +100,8 @@ function listenForPhotoSearchClick() {
         $('.page-buttons').empty();
         let searchTerm = $('#search-input').val();
         $('#search-input').val('');
-        console.log(searchTerm);
         getSearchResults(searchTerm, renderSearchResults);
+        
     });
 }
 
@@ -302,3 +309,116 @@ function callListeners() {
 }
 
 $(callListeners);
+
+// Particle Code 
+
+particlesJS("particle-container", {
+    "particles": {
+      "number": {
+        "value": 50,
+        "density": {
+          "enable": true,
+          "value_area": 1499.3805191013182
+        }
+      },
+      "color": {
+        "value": "random"
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 4
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.6333477640418815,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 48.10236182596568,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 24.36231636904035,
+          "size_min": 0,
+          "sync": true
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 2485.28869434156,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
